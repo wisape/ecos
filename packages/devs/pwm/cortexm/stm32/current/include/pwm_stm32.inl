9b8bde360bb74de0c765b7ea1b24d44fc93f0b60
Wisape Zhang <waiszhang@mountaintoptechnology.com>
2015-06-14 20:35:24 +0800

    pwm_stm32: add basic driver
    Signed-off-by: Wisape Zhang <waiszhang@mountaintoptechnology.com>

---
+++ packages/devs/pwm/cortexm/stm32/current/include/pwm_stm32.inl
@@ -0,0 +1,71 @@
+//==========================================================================
+//
+//      pwm_stm32.inl
+//
+//      STM32 PWM driver
+//
+//==========================================================================
+// ####ECOSGPLCOPYRIGHTBEGIN####
+// -------------------------------------------
+// This file is part of eCos, the Embedded Configurable Operating System.
+// Copyright (C) 2008 Free Software Foundation, Inc.
+//
+// eCos is free software; you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 2 or (at your option) any later
+// version.
+//
+// eCos is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with eCos; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+// As a special exception, if other files instantiate templates or use
+// macros or inline functions from this file, or you compile this file
+// and link it with other works to produce a work based on this file,
+// this file does not by itself cause the resulting work to be covered by
+// the GNU General Public License. However the source code for this file
+// must still be made available in accordance with section (3) of the GNU
+// General Public License v2.
+//
+// This exception does not invalidate any other reasons why a work based
+// on this file might be covered by the GNU General Public License.
+// -------------------------------------------
+// ####ECOSGPLCOPYRIGHTEND####
+//==========================================================================
+//#####DESCRIPTIONBEGIN####
+//
+// Author(s):    Wisape
+// Contributors:
+// Date:         2015-06-14
+// Purpose:
+// Description:
+//
+//####DESCRIPTIONEND####
+//
+//==========================================================================
+#ifndef CYG_STM32_PWM_INL
+#define CYG_STM32_PWM_INL
+
+DEVIO_TABLE(stm32_pwm_handles,
+	stm32_pwm_write,
+	stm32_pwm_read,
+	NULL,
+	stm32_pwm_get_config,
+	stm32_pwm_set_config
+);
+
+DEVTAB_ENTRY(stm32_pwm_io,
+	CYGDAT_DEVS_STM32_PWM_NAME,
+	NULL,
+	&stm32_pwm_handles,
+	stm32_pwm_init,
+	stm32_pwm_lookup,
+	NULL
+);
+
+#endif
---
+++ packages/devs/pwm/cortexm/stm32/current/src/pwm_stm32.c
@@ -0,0 +1,102 @@
+//==========================================================================
+//
+//      pwm_stm32.c
+//
+//      STM32 PWM driver
+//
+//==========================================================================
+// ####ECOSGPLCOPYRIGHTBEGIN####
+// -------------------------------------------
+// This file is part of eCos, the Embedded Configurable Operating System.
+// Copyright (C) 2008 Free Software Foundation, Inc.
+//
+// eCos is free software; you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 2 or (at your option) any later
+// version.
+//
+// eCos is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with eCos; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+// As a special exception, if other files instantiate templates or use
+// macros or inline functions from this file, or you compile this file
+// and link it with other works to produce a work based on this file,
+// this file does not by itself cause the resulting work to be covered by
+// the GNU General Public License. However the source code for this file
+// must still be made available in accordance with section (3) of the GNU
+// General Public License v2.
+//
+// This exception does not invalidate any other reasons why a work based
+// on this file might be covered by the GNU General Public License.
+// -------------------------------------------
+// ####ECOSGPLCOPYRIGHTEND####
+//==========================================================================
+//#####DESCRIPTIONBEGIN####
+//
+// Author(s):    Wisape
+// Contributors:
+// Date:         2015-06-14
+// Purpose:
+// Description:
+//
+//####DESCRIPTIONEND####
+//
+//==========================================================================
+#include <cyg/hal/hal_endian.h>
+#include <cyg/io/pwm_stm32.h>
+
+#include CYGDAT_DEVS_STM32_PWM_INL
+
+static Cyg_ErrNo stm32_pwm_write(cyg_io_handle_t handle,
+				const void *buf,
+				cyg_uint32 *len)
+{
+
+	return ENOERR;
+
+}
+
+static Cyg_ErrNo stm32_pwm_read(cyg_io_handle_t handle,
+				void *buf,
+				cyg_uint32 *len)
+{
+	return ENOERR;
+
+}
+
+static Cyg_ErrNo stm32_pwm_set_config(cyg_io_handle_t handle,
+				cyg_uint32 cmd,
+				const void *buf,
+				cyg_uint32 *len)
+{
+	return ENOERR;
+
+}
+
+static Cyg_ErrNo stm32_pwm_get_config(cyg_io_handle_t handle,
+				cyg_uint32 cmd,
+				void *buf,
+				cyg_uint32 *len)
+{
+	return ENOERR;
+
+}
+
+static Cyg_ErrNo stm32_pwm_lookup(struct cyg_devtab_entry **tab,
+				struct cyg_devtab_entry  *sub_tab,
+				const char *name)
+{
+	return ENOERR;
+
+}
+
+static bool stm32_pwm_init(struct cyg_devtab_entry *tab)
+{
+	return true;
+}
